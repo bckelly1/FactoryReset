93790d2eae0d9fc3cf9ebecfe99d4009dd69e66b
Brian Kelly <wolfyman06@gmail.com>
2020-09-18 14:48:47 -0600

    first commit

---
+++ src/main/java/com/example/examplemod/events/ClientEvents.java
@@ -0,0 +1,32 @@
+package com.example.examplemod.events;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ExampleMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientEvents {
+
+    @SubscribeEvent
+    public static void onJumpWithStick(PlayerInteractEvent.LeftClickBlock event) {
+        LivingEntity player = event.getEntityLiving();
+        if(player.getHeldItemMainhand().getItem() == Items.STICK) {
+            World world = player.getEntityWorld();
+
+            BlockPos blockPos = event.getPos();
+            if(world.getBlockState(blockPos).getBlock().getRegistryType() == Blocks.DIRT.getClass()) {
+                world.setBlockState(blockPos, Blocks.GRASS_PATH.getDefaultState());
+            }
+        }
+    }
+}
